5a46797ea57f214631ea0902f76ac9227b403b05
John Doe <jack.h.sawyer@pm.me>
2023-01-18 08:59:07 -0800

    first commit

---
+++ Condition.java
@@ -0,0 +1,12 @@
+class Condition {
+    public static void main(String[] args) {
+        boolean learning = true;
+
+        if (learning) {
+            System.out.println("Jaaaaaaaaaava PROGS!!!!!");
+        }
+        else {
+            System.out.println("You need to leave... NOW!!!");
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+A simple boolean... Thank you!
+
